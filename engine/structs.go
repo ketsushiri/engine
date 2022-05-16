e95716cbd5cd50289f4a1c3322e85fddb6f45ef4
emptiness <50295222+ketsushiri@users.noreply.github.com>
2022-05-16 16:54:55 +0300

    Basic database structs.

---
+++ engine/structs.go
@@ -0,0 +1,56 @@
+package engine
+
+import (
+	"fmt"
+)
+
+// Content.
+type Board struct {
+	BoardID uint64
+	Name    string
+
+	Threads    []Thread
+	LastPostID uint64
+}
+
+type Thread struct {
+	Posts []Post
+}
+
+type Post struct {
+	PostID uint64
+	OP     bool
+
+	Date  string
+	Theme string
+	Name  string
+
+	Files   []File
+	Comment string
+}
+
+type File struct {
+	FileID  uint64
+	Content []byte
+	FileExt string
+}
+
+func (f File) String() string {
+	return fmt.Sprintf("%d.%s", f.FileID, f.FileExt)
+}
+
+// Users.
+const (
+	ADMIN = iota
+	MODER
+	USER
+)
+
+type User struct {
+	UID             uint64
+	Login, Password string
+	AccessType      uint8
+
+	// ID досок, на которых юзер имеет права модератора.
+	ModRights []uint64
+}
